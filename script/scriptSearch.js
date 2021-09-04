824ad4344ca5e6fe1913e66c2a7e00bc9a6aa373
WRABZY <trawin@outlook.com>
2021-09-04 20:46:52 +0300

    changed search from yandex to duckduckgo

--- script/scriptSearch.js
+++ script/scriptSearch.js
@@ -1,7 +1,7 @@
 (function() {
   document.getElementById('searchBar').addEventListener('keydown', function(e) {
     if (e.keyCode === 13 && document.getElementById("searchBar").value != "") {
-      window.open("https://yandex.ru/search/?text=" + document.getElementById("searchBar").value);
+      window.open("https://duckduckgo.com/?q=" + document.getElementById("searchBar").value + "&t=hx&va=g&atb=v289-4gx&ia=web");
       document.getElementById("searchBar").value = "";
     }
   });
